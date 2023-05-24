804855479a4d4b3ab2539f457d62229451db5bbb
Mohamed Hashir <hashirhasmy111@gmail.com>
2023-05-24 21:57:09 +0530

    calculate the count of characted of name 01

--- src/main/java/com/lao/JavaPractice/test.java
+++ src/main/java/com/lao/JavaPractice/test.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 public class test {
     public static void main(String[] args) {
         name("hashir");
-        int x = 50;
-        int y = 10;
-        x = 100;
-        System.out.println(x=x+y);
-        System.out.println(x=x-y);
-        System.out.println("-------");
+//        int x = 50;
+//        int y = 10;
+//        x = 100;
+//        System.out.println(x=x+y);
+//        System.out.println(x=x-y);
+//        System.out.println("-------");
     }
 
     public static void name(String typed_name){
@@ -24,8 +24,21 @@ public class test {
             }
         }
         System.out.println(map);
-
     }
+
+    public static void name1(String typed_name){
+        HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+        char[] chartArray = typed_name.toCharArray();
+        for (char c:chartArray) {
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }else{
+                map.put(c,1);
+            }
+        }
+        System.out.println(map);
+    }
+
 
 
 }
